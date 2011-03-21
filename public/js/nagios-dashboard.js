fad980f2e9a318698695a8d9a19dd853136172f8
Justin Kolberg <justinkolberg@aelatis.com>
2011-03-21 11:38:37 -0700

    Added datetime formatting so that we don't need calculators to convert the unixtime.

--- public/js/nagios-dashboard.js
+++ public/js/nagios-dashboard.js
@@ -6,15 +6,20 @@ $(document).ready(function(){
 			data = JSON.parse(evt.data);
 
 			for(var msg in data) {
+				
 				var status = '';
 				if(data[msg]['status'] == 'CRITICAL') {
 					status = "Critical";
 				} else if (data[msg]['status'] == 'WARNING') {
 					status = "Warning";
 				}
+				
+				var last_time_ok = new Date(data[msg]['last_time_ok'] * 1000);
+				var last_check = new Date(data[msg]['last_check'] * 1000);
+				
 				$("#messages").append('<tr class="'+status+'"><td>'+data[msg]['host_name']
-					+'</td><td>'+data[msg]['plugin_output']+'</td><td>'+data[msg]['last_time_ok']
-					+'</td><td>'+data[msg]['last_check']+'</td></tr>');
+					+'</td><td>'+data[msg]['plugin_output']+'</td><td>'+last_time_ok.toLocaleString()
+					+'</td><td>'+last_check.toLocaleString()+'</td></tr>');
 			};
 		};
 		ws.onclose = function() { debug("socket closed"); };
