3d2d1d727a7037e941faaecd48d0db8b506c16c6
Sean Porter <portertech@gmail.com>
2011-03-23 15:06:39 -0700

    started talking to the opscode platform api

--- dashboard.rb
+++ dashboard.rb
@@ -1,64 +1,97 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'optparse'
 require 'logger'
-
-@options = {}
-optparse = OptionParser.new do |opts|
-  `which nagios-dashboard`; opts.banner = case $?.exitstatus
-  when 0
-    "Usage: nagios-dashboard [options]"
-  else
-    "Usage: dashboard.rb [options]"
-  end
-
-  @options[:verbose] = false
-  opts.on('-v', '--verbose', 'Output debug messages to screen') do
-    @options[:verbose] = true
-  end
-
-  @options[:datfile] = "/var/cache/nagios3/status.dat"
-  opts.on('-d', '--datfile FILE', 'Location of Nagios status.dat FILE (default: /var/cache/nagios3/status.dat)') do |file|
-    @options[:datfile] = file
-  end
-
-  @options[:port] = 80
-  opts.on('-p', '--port PORT', 'Listen on a different PORT (default: 80)') do |port|
-    @options[:port] = port
-  end
-
-  @options[:logfile] = File.dirname(__FILE__) + '/debug.log'
-  opts.on('-l', '--logfile FILE', 'Write log messages to FILE (default: ./debug.log)') do |file|
-    @options[:logfile] = file
-  end
-
-  opts.on('-h', '--help', 'Display this screen') do
-    puts opts
-    exit
-  end
- end
-optparse.parse!
-
-@log = Logger.new(@options[:logfile])
-@log.debug('starting dashboard ...')
-
-require "bundler/setup"
+require 'bundler/setup'
 Bundler.require(:default)
+
+class Options
+  include Mixlib::CLI
+  option :verbose,
+    :short => '-v',
+    :long  => '--verbose',
+    :boolean => true,
+    :default => false,
+    :description => 'Output debug messages to screen'
+
+  option :datfile,
+    :short => '-d FILE',
+    :long  => '--datfile FILE',
+    :default => '/var/cache/nagios3/status.dat',
+    :description => 'Location of Nagios status.dat FILE'
+
+  option :port,
+    :short => '-p PORT',
+    :long  => '--port PORT',
+    :default => 80,
+    :description => 'Listen on a different PORT'
+
+  option :logfile,
+    :short => '-l FILE',
+    :long  => '--logfile FILE',
+    :default => File.dirname(__FILE__) + '/debug.log',
+    :description => 'Listen on a different PORT'
+
+  option :user,
+    :short => '-u USER',
+    :long  => '--user USER',
+    :description => 'OpsCode plaform USER (required)'
+
+  option :key,
+    :short => '-k KEY',
+    :long  => '--key KEY',
+    :description => 'OpsCode plaform user KEY (required)'
+
+  option :organization,
+    :short => '-o ORGANIZATION',
+    :long  => '--organization ORGANIZATION',
+    :description => 'OpsCode platform ORGANIZATION (required)'
+
+  option :help,
+    :short => "-h",
+    :long => "--help",
+    :description => "Show this message",
+    :on => :tail,
+    :boolean => true,
+    :show_options => true,
+    :exit => 0
+end
+
+OPTIONS = Options.new
+OPTIONS.parse_options
+
+@log = Logger.new(OPTIONS.config[:logfile])
+@log.debug('starting dashboard ...')
 
 EventMachine.epoll if EventMachine.epoll?
 EventMachine.kqueue if EventMachine.kqueue?
 EventMachine.run do
   class Dashboard < Sinatra::Base
-    set :logging, true
+    register Sinatra::Async
     set :static, true
     set :public, 'public'
-    get '/' do
-      haml :dashboard
+
+    Spice.setup do |s|
+      s.host = "api.opscode.com"
+      s.port = 443
+      s.scheme = "https"
+      s.url_path = 'organizations/' + OPTIONS.config[:organization]
+      s.client_name = OPTIONS.config[:user]
+      s.key_file = OPTIONS.config[:key]
+    end
+    Spice.connect!
+
+    aget '/' do
+      body haml :dashboard
+    end
+
+    aget '/chef' do
+      content_type 'application/json'
+      body Spice.connection.get("/nodes")
     end
   end
 
   def log_message(message)
-    if @options[:verbose]
+    if OPTIONS.config[:verbose]
       puts message
     end
     EventMachine.defer(proc{@log.debug(message)})
@@ -78,7 +111,7 @@ EventMachine.run do
 
   nagios_status = proc do
     begin
-      nagios = NagiosAnalyzer::Status.new(@options[:datfile])
+      nagios = NagiosAnalyzer::Status.new(OPTIONS.config[:datfile])
       log_message('parsed nagios status.dat')
       nagios.items.to_json
     rescue => error
@@ -93,11 +126,11 @@ EventMachine.run do
     log_message('updated clients')
   end
 
-  EMDirWatcher.watch File.dirname(File.expand_path(@options[:datfile])), :include_only => ['status.dat'] do
+  EMDirWatcher.watch File.dirname(File.expand_path(OPTIONS.config[:datfile])), :include_only => ['status.dat'] do
     EventMachine.defer(nagios_status, update_clients)
   end
 
-  Dashboard.run!({:port => @options[:port]})
+  Dashboard.run!({:port => OPTIONS.config[:port]})
 end
 
 @log.debug('stopping dashboard ...')
