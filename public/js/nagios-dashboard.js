6d7e5ab6b96ec916b7e29be8573fc4159044b014
Justin Kolberg <justinkolberg@aelatis.com>
2011-03-25 10:29:50 -0700

    Added popups for more information.

--- public/js/nagios-dashboard.js
+++ public/js/nagios-dashboard.js
@@ -13,9 +13,28 @@ $(document).ready(function(){
       }
       var last_time_ok = new Date(data[msg]['last_time_ok'] * 1000);
       var last_check = new Date(data[msg]['last_check'] * 1000);
+      
       $("#messages").append('<tr class="'+status+'"><td>'+data[msg]['host_name']
-        +'</td><td>'+data[msg]['plugin_output']+'</td><td>'+last_time_ok.toLocaleString()
+        +'</td><td>'+data[msg]['plugin_output']+'<div style="display: none;">'
+        +'<div style="width:400px;height:100px;overflow:auto;">'
+        +
+        +'</div></div>'
+        +'</td><td>'+last_time_ok.toLocaleString()
         +'</td><td>'+last_check.toLocaleString()+'</td></tr>');
+      
+      $("#messages > tr:last").click(function() {
+        $.fancybox({
+		  //'orig'			: $(this),
+		  'padding'		    : 0,
+          'content'         : '<strong>Plugin Output: </strong><pre>'
+            +data[msg]['long_plugin_output']+'</pre><br />'
+            +'<strong>Performance Data: </strong><pre>'+data[msg]['performance_data']+'</pre><br />'
+            +'<strong>Check Command: </strong><pre>'+data[msg]['check_command']+'</pre>',
+		  'title'   		: data[msg]['host_name'],
+		  'transitionIn'	: 'elastic',
+		  'transitionOut'	: 'elastic'
+	    });
+      });
     };
   };
   ws.onclose = function() { debug("socket closed"); };
