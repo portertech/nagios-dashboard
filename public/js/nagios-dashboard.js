86f287b01e740fbc135626763bf5c3e2b478d0a8
Justin Kolberg <justinkolberg@aelatis.com>
2011-04-05 16:04:30 -0700

    Fixed popups.

--- public/js/nagios-dashboard.js
+++ public/js/nagios-dashboard.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   ws.onmessage = function(evt) {
     $("#messages").empty();
     data = JSON.parse(evt.data);
+    var i = 0;
+    
     for(var msg in data) {
+
       var status = '';
       if(data[msg]['status'] == 'CRITICAL') {
         status = "Critical";
@@ -14,25 +17,29 @@ $(document).ready(function(){
       var last_time_ok = new Date(data[msg]['last_time_ok'] * 1000);
       var last_check = new Date(data[msg]['last_check'] * 1000);
       
-      $("#messages").append('<tr class="'+status+'"><td>'+data[msg]['host_name']
+      $("#messages").append('<tr class="'+status+'" id="link_'+i+'" href="#popup_'+i+'"><td>'+data[msg]['host_name']
         +'</td><td>'+data[msg]['plugin_output']+'<div style="display: none;">'
         +'<div style="width:400px;height:100px;overflow:auto;">'
         +
         +'</div></div>'
         +'</td><td>'+last_time_ok.toLocaleString()
-        +'</td><td>'+last_check.toLocaleString()+'</td></tr>').click(function() {
-        $.fancybox({
-		  //'orig'			: $(this),
-		  'padding'		    : 5,
-          'content'         : '<strong>Plugin Output: </strong><pre>'
-            +data[msg]['long_plugin_output']+'</pre><br />'
-            +'<strong>Performance Data: </strong><pre>'+data[msg]['performance_data']+'</pre><br />'
-            +'<strong>Check Command: </strong><pre>'+data[msg]['check_command']+'</pre>',
-		  'title'   		: data[msg]['host_name'],
-		  'transitionIn'	: 'elastic',
-		  'transitionOut'	: 'elastic'
-	    });
+        +'</td><td>'+last_check.toLocaleString()+'</td></tr>');
+      
+      $("#popups_container").append('<div id="popup_'+i+'">'
+      +'<strong>Plugin Output: </strong><pre>'
+      +data[msg]['long_plugin_output']+'</pre><br />'
+      +'<strong>Performance Data: </strong><pre>'+data[msg]['performance_data']+'</pre><br />'
+      +'<strong>Check Command: </strong><pre>'+data[msg]['check_command']+'</pre>'
+      +'</div>');
+      
+      $("#link_"+i).fancybox({
+           'title'         : data[msg]['host_name'],
+           'padding'       : 5,
+           'transitionIn'  : 'elastic',
+           'transitionOut' : 'elastic'
       });
+      
+      i++;
     };
   };
   ws.onclose = function() { debug("socket closed"); };
